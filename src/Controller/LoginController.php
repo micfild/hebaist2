e11dcffd55597f572cb894f98a29d30231ea59ea
micfild <43708100+micfild@users.noreply.github.com>
2019-02-11 18:25:44 +0100

    idem pour login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -14,6 +14,10 @@ class LoginController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('logged');
+        }
+        
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
