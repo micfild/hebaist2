a0ef5dc76056c73c500976463cb3597625f49a17
micfild <43708100+micfild@users.noreply.github.com>
2019-02-11 18:23:59 +0100

    modif controller
    ajout d'une condition pour rediriger vers route logged depuis register quand on est deja loggé

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -19,6 +19,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginAuthenticator $authenticator): Response
     {
+        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('logged');
+        }
+
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
